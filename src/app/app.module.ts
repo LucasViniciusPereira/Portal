13cef719b862549c7f1ca194e0cf02dacb1a744b
lucasviniciuspereira <lucasvp29@hotmail.com>
2017-09-12 22:34:23 -0300

    use ng2-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './areas/home/login/login.component';
 
+import { MdInputModule } from '@angular/material';
+import { SharedModule } from './shared.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdInputModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
